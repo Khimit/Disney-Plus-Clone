c11df2b0847b4c2595319628ca179d9ff985cadf
khimit <khimitjangale@gmail.com>
2024-02-20 17:27:51 +0530

    added env final done

--- src/firebase.js
+++ src/firebase.js
@@ -2,17 +2,26 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
-import {getStorage} from "firebase/storage"
+import { getStorage } from "firebase/storage"
+
+const VITE_FB_APIKEY = import.meta.env.VITE_FB_APIKEY;
+const VITE_FB_AUTHDOMAIN = import.meta.env.VITE_FB_AUTHDOMAIN;
+const VITE_FB_PROJECTID = import.meta.env.VITE_VITE_FB_PROJECTID;
+const VITE_FB_STORAGEBUCKET = import.meta.env.VITE_VITE_FB_STORAGEBUCKET;
+const VITE_FB_MESSAGINGSENDERID = import.meta.env.VITE_VITE_FB_MESSAGINGSENDERID;
+const VITE_FB_APPID = import.meta.env.VITE_VITE_FB_APPID;
+const VITE_FB_MEASUREMENTID = import.meta.env.VITE_VITE_FB_MEASUREMENTID;
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAVJLb7_uXIp6R4FkB463xfYs6l9PySbPw",
-  authDomain: "disneyplus-cd068.firebaseapp.com",
-  projectId: "disneyplus-cd068",
-  storageBucket: "disneyplus-cd068.appspot.com",
-  messagingSenderId: "319304671126",
-  appId: "1:319304671126:web:94ffa6bb566b15b0c507b7",
-  measurementId: "G-5TSXFBKJR6",
+  apiKey: VITE_FB_APIKEY,
+  authDomain: VITE_FB_AUTHDOMAIN,
+  projectId: VITE_FB_PROJECTID,
+  storageBucket: VITE_FB_STORAGEBUCKET,
+  messagingSenderId: VITE_FB_MESSAGINGSENDERID,
+  appId: VITE_FB_APPID,
+  measurementId: VITE_FB_MEASUREMENTID,
 };
+
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
